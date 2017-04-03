3469465bdb4c5e0a9b50d09e74733213a7068617
Max Collins <Max Collins>
2017-04-03 18:14:17 -0400

    Homework for 4-3

---
+++ class-work/class08/first-javascript/script.js
@@ -0,0 +1,19 @@
+// document.write(10 * 10)
+// document.write(10 / 10)
+// document.write(50 - 18)
+// document.write(50 + 17)
+// document.write("Max " + "Collins")
+// var x = 2;
+// var x = x * 2;
+// var num1 = 10 * 10;
+// document.write(num1);
+// document.write(x);
+
+
+// // to run a function, you have to put run()
+// //if you just put 'run' then it will literally paste your function.
+
+var title = document.querySelector('#title') 
+
+document.querySelector('#title').style.color = 'red'
+document.querySelectorAll('h1').style.color = 'green'
